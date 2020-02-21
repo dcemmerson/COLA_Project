e0fa3395b4e5efc6e0d52efe4ed11f6dd60e0b97
Shifra T Schectman <43650010+schectms@users.noreply.github.com>
2020-02-21 14:25:01 -0500

    Update createtables.sql

--- sql/createtables.sql
+++ sql/createtables.sql
@@ -9,28 +9,28 @@ CREATE TABLE users (
 	created datetime NOT NULL,
 	modified datetime NOT NULL,
 	PRIMARY KEY (id),
-    UNIQUE (email)
+    	UNIQUE (email)
 );
 
 CREATE TABLE template (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-    file blob NOT NULL,
-    comment text,
+	file blob NOT NULL,
+  	comment text,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE subscription (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-    comment text,
+    	comment text,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE cola_rates (
 	id int NOT NULL AUTO_INCREMENT,
 	country varchar(255) NOT NULL,
-    post varchar(255) NOT NULL,
-    allowance int NOT NULL,
+    	post varchar(255) NOT NULL,
+    	allowance int NOT NULL,
 	PRIMARY KEY (id)
 );
