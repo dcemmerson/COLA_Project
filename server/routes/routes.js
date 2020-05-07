9a63a465185520128a0240317cc0858f99c42926
Dane Emmerson <emmersod@oregonstate.edu>
2020-05-06 23:00:23 -0700

    redesigning faq page, minor footer adjustments

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -15,6 +15,7 @@ module.exports = function (app) {
 
 	context.title = 'COLA Notifications';
 	context.homepage = true;
+
 	
 	misc.set_layout(req, context)
 	    .catch(() => console.log('error in set_layout'))
@@ -67,13 +68,13 @@ module.exports = function (app) {
 	    .finally(() => res.render('reset', context))
     });
 
-    app.get('/FAQ', function (req, res) {
+    app.get('/about', function (req, res) {
 	let context = {};
 	context.style = ['styles.css', 'font_size.css', 'FAQ.css'];
 	context.script = ['FAQ.js', 'utility.js'];
 	context.deferScript = ['../pdfjs/pdf.js'];
-	context.title = 'FAQ - COLA';
-	context.FAQ = true;
+	context.title = 'About - COLA';
+	context.about = true;
 
 	misc.set_layout(req, context)
 	    .catch(() => console.log('error in set_layout'))
@@ -129,65 +130,6 @@ module.exports = function (app) {
 	Promise.all(awaitPromises)
 	    .then(() => res.render('subscriptions', context))
     });
-    
- /*   // upon submitting create account, validates the form information and adds user to DB
-    app.post(
-	"/create_account", [
-	    // Check validity+
-	    check("email", "Email is invalid")
-		.isEmail()
-		.isLength({
-		    min: 4
-		}),
-	    check("pwd", "Password is invalid: must be at least 8 characters and must contain 1 lowercase, 1 uppercase, 1 number and 1 special character")
-		.isLength({
-		    min: 8
-		})
-		.matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, 'i')
-		.custom((value, {
-		    req,
-		    loc,
-		    path
-		}) => {
-		    if (value !== req.body.pwdmatch) {
-			throw new Error("Passwords don't match");
-		    } else {
-			return value;
-		    }
-		})
-	],
-	(req, res, next) => {
-	    // return formatted validation results
-	    const errorFormatter = ({
-		msg,
-	    }) => {
-		return `${msg}`;
-	    };
-	    const errors = validationResult(req).formatWith(errorFormatter);
-
-	    if (!errors.isEmpty()) {
-		var errorResponse = errors.array({
-		    onlyFirstError: true
-		});
-		res.render('create', {
-		    error: errorResponse
-		});
-
-		return;
-	    }
-
-	    //if no errors, add user to DB
-	    else {
-		var email = req.body.email;
-		var pwd = req.body.pwd;
-		var now = new Date().toISOString().replace(/\..+/, '');
-		db.add_user(email, pwd, now, res, req)
-		.then(console.log(res[0].email))
-		.catch(err => console.log(err))
-		
-		}
-
-	})*/
    
     app.get(`/logout`, function (req, res) {
 	req.logout();
