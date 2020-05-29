18f861ba8bfb4cfe57916276d0c4bd833a57ca3e
Dane Emmerson <emmersod@oregonstate.edu>
2020-05-28 19:44:22 -0700

    Added a warning message to appear for non- Firefox/Chrome/Safari browsers until we resolve the issues with browser compability.

--- server/public/js/utility.js
+++ server/public/js/utility.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     displayReturnToTop();
+    displayBrowserWarning();
 })
 function hiddenTimer(element, time = 5000) {
     setTimeout(() => {
@@ -107,7 +108,7 @@ function removeClasses(elements, classes) {
 
 function hideElements(elements) {
     for (let i = 0; i < elements.length; i++) {
-        elements[i].style.display = 'none';
+        elements[i].style.display= 'none';
     }
 }
 function clearInnerText(elements) {
@@ -130,6 +131,87 @@ function displayReturnToTop() {
         rtt.style.display = 'block';
     }
 }
+
+function displayBrowserWarning(){
+    // https://medium.com/creative-technology-concepts-code/detect-device-browser-and-version-using-javascript-8b511906745
+    var os = [
+	{ name: 'Windows Phone', value: 'Windows Phone', version: 'OS' },
+	{ name: 'Windows', value: 'Win', version: 'NT' },
+	{ name: 'iPhone', value: 'iPhone', version: 'OS' },
+	{ name: 'iPad', value: 'iPad', version: 'OS' },
+	{ name: 'Kindle', value: 'Silk', version: 'Silk' },
+	{ name: 'Android', value: 'Android', version: 'Android' },
+	{ name: 'PlayBook', value: 'PlayBook', version: 'OS' },
+	{ name: 'BlackBerry', value: 'BlackBerry', version: '/' },
+	{ name: 'Macintosh', value: 'Mac', version: 'OS X' },
+	{ name: 'Linux', value: 'Linux', version: 'rv' },
+	{ name: 'Palm', value: 'Palm', version: 'PalmOS' }
+    ];
+    var browser = [
+	{ name: 'Chrome', value: 'Chrome', version: 'Chrome' },
+	{ name: 'Firefox', value: 'Firefox', version: 'Firefox' },
+	{ name: 'Safari', value: 'Safari', version: 'Version' },
+	{ name: 'Internet Explorer', value: 'MSIE', version: 'MSIE' },
+	{ name: 'Opera', value: 'Opera', version: 'Opera' },
+	{ name: 'BlackBerry', value: 'CLDC', version: 'CLDC' },
+	{ name: 'Mozilla', value: 'Mozilla', version: 'Mozilla' }
+    ];
+    var header = [
+	navigator.platform,
+	navigator.userAgent,
+	navigator.appVersion,
+	navigator.vendor,
+	window.opera
+    ];
+
+    var agent = header.join(' ');
+    var os = matchItem(agent, os);
+    var browser = matchItem(agent, browser);
+
+    if(browser.name === 'Chrome' || browser.name === 'Firefox' || browser.name === "Safari"){
+//	document.getElementById("browserWarning").setAttribute("style", "display: none");
+    }
+    
+    function matchItem(string, data) {
+	var i = 0,
+            j = 0,
+            html = '',
+            regex,
+            regexv,
+            match,
+            matches,
+            version;
+	
+	for (i = 0; i < data.length; i += 1) {
+            regex = new RegExp(data[i].value, 'i');
+            match = regex.test(string);
+            if (match) {
+		regexv = new RegExp(data[i].version + '[- /:;]([\d._]+)', 'i');
+		matches = string.match(regexv);
+		version = '';
+		if (matches) { if (matches[1]) { matches = matches[1]; } }
+		if (matches) {
+                    matches = matches.split(/[._]+/);
+                    for (j = 0; j < matches.length; j += 1) {
+			if (j === 0) {
+                            version += matches[j] + '.';
+			} else {
+                            version += matches[j];
+			}
+                    }
+		} else {
+                    version = '0';
+		}
+		return {
+                    name: data[i].name,
+                    version: parseFloat(version)
+		};
+            }
+	}
+	return { name: 'unknown', version: 0 };
+    }
+}
+
 function disableElements(elements) {
     for (let i = 0; i < elements.length; i++) {
         elements[i].disabled = true;
