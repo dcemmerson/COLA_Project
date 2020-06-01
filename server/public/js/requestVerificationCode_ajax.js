b7fb08ddb1133fbfba9582cf7c8ff92ebb152655
Dane Emmerson <emmersod@oregonstate.edu>
2020-05-31 21:10:26 -0700

    Cleaning up public/js dir to remove src files.

--- server/public/js/requestVerificationCode_ajax.js
+++
@@ -1,9 +0,0 @@
-function submitRequest(email) {
-    return fetch('/requestVerificationCode', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username: email })
-    })
-}
--- server/public/js/reset_ajax.js
+++
@@ -1,9 +0,0 @@
-function submitRequest(email) {
-    return fetch('/reset', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email: email })
-    })
-}
